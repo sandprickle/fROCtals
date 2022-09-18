e668ee506a9ea0fc122c747f8112b3714e51b9c8
Bryce Miller <sandprickle@users.noreply.github.com>
2022-09-17 20:00:09 -0400

    haskell wip

--- mandelbrot.hs
+++ mandelbrot.hs
@@ -2,26 +2,44 @@
 width = 180
 height = 40
 maxIterations = 250
-xBounds = (-2.0, 2.0)
-yBounds = (-1.0, 1.0)
+xBounds = Bounds (-2.0, 2.0)
+yBounds = Bounds (-1.0, 1.0)
 center = Complex (0.0, 0.0)
 
-xScale = ( snd xBounds + fst xBounds ) / width
-yScale = ( snd yBounds + fst yBounds ) / height
+xScale = (bMax xBounds + bMin xBounds) / width
+yScale = (bMax yBounds + bMin yBounds) / height
 
+mandelbrot :: Complex -> Complex -> Integer -> Bool
+mandelbrot c z i
+    | i >= maxIterations = True
+    | re z3 * re z3 + im z3 * im z3 > 4.0 = False
+    | otherwise = mandelbrot c z3 (i + 1)
+  where
+    z2 =
+        Complex
+            ( re z * re z - im z - im z
+            , 2.0 * re z * im z
+            )
+    z3 =
+        Complex
+            ( re z2 + re c
+            , im z2 + im c
+            )
 
-mandelbrot :: Complex-> Complex -> Integer -> Bool
-mandelbrot c z i  
-    | i <= maxIterations = True
-    | otherwise = False
-    
-
-
-
+-- Complex Type & functions
 data Complex = Complex (Double, Double)
 
-real :: Complex -> Double
-real (Complex (re, _)) = re
+re :: Complex -> Double
+re (Complex (re, _)) = re
 
-imaginary :: Complex -> Double
-imaginary (Complex (_, im)) = im
+im :: Complex -> Double
+im (Complex (_, im)) = im
+
+-- Bounds Type & functions
+data Bounds = Bounds (Double, Double)
+
+bMin :: Bounds -> Double
+bMin (Bounds (min, _)) = min
+
+bMax :: Bounds -> Double
+bMax (Bounds (_, max)) = max
