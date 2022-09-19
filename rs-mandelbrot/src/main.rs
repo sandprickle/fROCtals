146c84cd48c5295a5f560ea6fe13153f07ef97ac
Bryce Miller <sandprickle@users.noreply.github.com>
2022-09-19 00:28:03 -0400

    accept iter exponent arg

--- rs-mandelbrot/src/main.rs
+++ rs-mandelbrot/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 const CONFIG: Config = Config {
     x_bounds: Bounds {
         min: -2.0,
@@ -8,7 +10,8 @@ const CONFIG: Config = Config {
         max: 1.0,
     },
     center: Complex { re: -0.5, im: 0.0 },
-    max_iterations: 1_000,
+    iter_base: 10,
+    iter_default_exp: 3,
     height: 40,
     width: 180,
 };
@@ -17,6 +20,19 @@ const X_SCALE: f64 = (CONFIG.x_bounds.max - CONFIG.x_bounds.min) / (CONFIG.width
 const Y_SCALE: f64 = (CONFIG.y_bounds.max - CONFIG.y_bounds.min) / (CONFIG.height as f64);
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let max_iter_exponent = if args.len() > 1 {
+        match args[1].parse::<u32>() {
+            Ok(int) => int,
+            Err(_) => CONFIG.iter_default_exp,
+        }
+    } else {
+        CONFIG.iter_default_exp
+    };
+
+    let max_iterations = CONFIG.iter_base.pow(max_iter_exponent);
+
     let mut grid = [['?'; CONFIG.width]; CONFIG.height];
 
     for (row_i, row) in grid.into_iter().enumerate() {
@@ -26,7 +42,11 @@ fn main() {
                 im: row_i as f64 * Y_SCALE + CONFIG.y_bounds.min + CONFIG.center.im,
             };
 
-            grid[row_i][col_i] = if mandelbrot(c) { '*' } else { ' ' }
+            grid[row_i][col_i] = if mandelbrot(c, max_iterations) {
+                '*'
+            } else {
+                ' '
+            }
         }
     }
 
@@ -35,11 +55,11 @@ fn main() {
     }
 }
 
-fn mandelbrot(c: Complex) -> bool {
+fn mandelbrot(c: Complex, max_iterations: u32) -> bool {
     let mut z = Complex { re: 0.0, im: 0.0 };
     let mut iter_count = 0;
 
-    while z.re + z.im <= 4.0 && iter_count < CONFIG.max_iterations {
+    while z.re + z.im <= 4.0 && iter_count < max_iterations {
         z = Complex {
             re: z.re * z.re - z.im * z.im,
             im: 2.0 * z.re * z.im,
@@ -53,7 +73,7 @@ fn mandelbrot(c: Complex) -> bool {
         iter_count += 1;
     }
 
-    iter_count == CONFIG.max_iterations
+    iter_count == max_iterations
 }
 struct Complex {
     re: f64,
@@ -69,7 +89,8 @@ struct Config {
     x_bounds: Bounds,
     y_bounds: Bounds,
     center: Complex,
-    max_iterations: usize,
+    iter_base: u32,
+    iter_default_exp: u32,
     height: usize,
     width: usize,
 }
